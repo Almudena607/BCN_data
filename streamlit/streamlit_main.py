74a71bc6b5ba987f38bc527e7261ca066ddf5e87
Almudena <almudenaramirez607@gmail.com>
2022-11-16 15:01:18 +0000

    routers y streamlit

---
+++ streamlit/streamlit_main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Inmigración y desempleo en los barrios de Barcelona")
+st.text("Este proyecto pretende mostrar la tasa de desempleo e inmigración en relación con\nla población total de Barcelona.")
+st.text("Para ello se toman como variables independientes los barrios o distritos de\nBarcelona, y como variables independientes, la cantidad de población pertenecientes\nal grupo de inmigrantes, desempleados o el total.\nTambién se tendrá en cuenta el año de recogida de datos.")
+
+st.header("Variables")
+
