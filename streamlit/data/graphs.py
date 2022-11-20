7893a429c84acefa4c3ae04a2afa084e19c0c177
Almudena <almudenaramirez607@gmail.com>
2022-11-20 18:21:49 +0000

    streamlit casi terminado

--- streamlit/data/graphs.py
+++ streamlit/data/graphs.py
@@ -1,50 +1,150 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-from data.st_data import all_neigh, get_neighborhoods, get_immigrants_nationality
-import seaborn as sns
+from data.st_data import get_neighborhoods, get_immigrants_nationality, all_nationality, get_immigrants_neighborhood, all_district, get_unemployed_neighborhood
 import plotly.graph_objs as go
 import plotly.express as pe
-import plotly.figure_factory as ff
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-import numpy as np
 
 #--------------------------------------------GRÁFICOS INMIGRACIÓN------------------------------- 
-def graph_inmigrantes():
-    nationality = st.text_input('Write the name of the country in English (e.g.: Spain)')
-    df = pd.DataFrame.from_dict(get_immigrants_nationality(nationality))                ###### !!!! ValueError: If using all scalar values, you must pass an index
+########!!!!!!!! 
+def barrio_inmigrantes():
+
+    #boxes to select the district and the page
+    distrito = st.selectbox("Selecciona el distrito:", all_district())
+    pag_lines = [0,1,2,3,4,5,6,7] 
+    n = st.selectbox("Seleccsjfgjshkgb los datos de la inmigración de empleo en función del barrio:", pag_lines)
+
+
+    #getting the data
+    a = get_immigrants_neighborhood(distrito, n)
+    df = pd.DataFrame(a)
     distritos = []
     inmigrantes = []
     nacionalidad = []
     barrio = []
-    for d in df["results"]:
-        for v in d:
-            for i in v:
-                if "Nationality" == i:
-                    distritos.append(v["District Name"])
-                elif "Number" == i:
-                    inmigrantes.append(v["Number"])
-                elif "Neighborhood Name" == i:
-                    barrio.append(v["Neighborhood Name"])
-                elif "Nationality" == i:
-                    nacionalidad.append(v["Nationality"])
+    for _, data in df.iterrows():
+        for dicc in df["results"]:
+            for c in dicc:
+                if "Number" == c:
+                    inmigrantes.append(dicc["Number"])
+                elif "Neighborhood Name" == c:
+                    barrio.append(dicc["Neighborhood Name"])
+                elif "Nationality" == c:
+                    nacionalidad.append(dicc["Nationality"])
+                elif "District Name" == c:
+                    distritos.append(dicc["District Name"])
 
-    fig = pe.line(x=nacionalidad, y=inmigrantes)
-
-    st.write(fig)
+    #drawing the graphs
     
+    fig = pe.bar(df, x=barrio, y=inmigrantes, color=nacionalidad)   
+    st.write(fig)
+
+
+
+def distrito_inmigrantes():
+
+    #boxes to select the nationality and the page
+    nationality = st.selectbox("Selecciona el país para ver los datos de esa nacionalidad:", all_nationality())
+    pag_lines = [0,1,2,3,4,5,6,7] 
+    n = st.selectbox("Selecciona la página para bhsjfks los datos de inmigrantes según el distrito:", pag_lines)
+
+    #getting the data
+    a = get_immigrants_nationality(nationality, n)
+    df = pd.DataFrame(a)
+    distritos = []
+    inmigrantes = []
+    barrio = []
+    nacionalidad = []
+    for _, data in df.iterrows():
+        for dicc in df["results"]:
+            for c in dicc:
+                if c == "District Name":
+                    distritos.append(dicc["District Name"])
+                elif "Number" == c:
+                    inmigrantes.append(dicc["Number"])
+                elif "Neighborhood Name" == c:
+                    barrio.append(dicc["Neighborhood Name"])
+                elif "Nationality" == c:
+                    nacionalidad.append(dicc["Nationality"])
+
+    #drawing the graphs
+    
+    fig = pe.bar(df, x=distritos, y=inmigrantes, color=barrio, labels={'x':'Distrito', 'y':'Tasa inmigración'})
+    st.write(fig)
+
+
+#--------------------------------------------GRÁFICOS DESEMPLEO-------------------------------
+
+def barrio_desempleo():
+
+    distrito = st.selectbox("Selecciona mdfbn mc distrito:", all_district())
+    pag_lines = [0,1,2,3,4,5,6,7] 
+    n = st.selectbox("Selecciona la fhjdscbhsak para ver los datos de la demanda de empleo en función del barrio:", pag_lines)
+
+    a = get_unemployed_neighborhood(distrito, n)
+    df = pd.DataFrame(a)
+    distritos = []
+    barrio = []
+    genero = []
+    desempleados = []
+    demanda_empleo = []
+    for _, data in df.iterrows():
+        for dicc in df["results"]:
+            for c in dicc:
+                if c == "District Name":
+                    distritos.append(dicc["District Name"])
+                elif "Number" == c:
+                    desempleados.append(dicc["Number"])
+                elif "Neighborhood Name" == c:
+                    barrio.append(dicc["Neighborhood Name"])
+                elif "Gender" == c:
+                    genero.append(dicc["Gender"])     
+                elif "Demand_occupation" == c:
+                    demanda_empleo.append(dicc["Demand_occupation"])
+
+    fig = pe.bar(df, x=barrio, y=desempleados, color=demanda_empleo, labels={'x':'Barrio', 'y': 'Tasa desempleo'})               
+    st.write(fig)
+
+def genero_desempleo():
+
+    distrito = st.selectbox("Selecdfjgnlkciona mdfbn mc distrito:", all_district())
+    pag_lines = [0,1,2,3,4,5,6,7] 
+    n = st.selectbox("Seleccionadsijklhjdscbhsak para ver los datos de la demanda de empleo en función del barrio:", pag_lines)
+
+    a = get_unemployed_neighborhood(distrito, n)
+    df = pd.DataFrame(a)
+    distritos = []
+    barrio = []
+    genero = []
+    desempleados = []
+    demanda_empleo = []
+    for _, data in df.iterrows():
+        for dicc in df["results"]:
+            for c in dicc:
+                if c == "District Name":
+                    distritos.append(dicc["District Name"])
+                elif "Number" == c:
+                    desempleados.append(dicc["Number"])
+                elif "Neighborhood Name" == c:
+                    barrio.append(dicc["Neighborhood Name"])
+                elif "Gender" == c:
+                    genero.append(dicc["Gender"])     
+                elif "Demand_occupation" == c:
+                    demanda_empleo.append(dicc["Demand_occupation"])
+
+    fig = pe.bar(df, x=barrio, y=desempleados, color=genero, labels={'x':'Barrio', 'y': 'Tasa desempleo'})               
+    st.write(fig)
+
 
 
 #--------------------------------------------GRÁFICOS RELACIÓN VARIABLES-------------------------------
 
-# poner tres columnas con tablas?
-
 def inmi_barrio():
     #box to select the page
     pag_lines = [0,1,2,3,4,5,6,7] 
-    n = st.selectbox("Selecciona la página para ver los datos de inmigrantes según el barrio:", pag_lines)
+    n = st.selectbox("Selecciona la pág para ver los datos de inmigrantes según el barrio:", pag_lines)
 
     #getting the data
     df = pd.DataFrame.from_dict(get_neighborhoods(n))
@@ -101,7 +201,7 @@ def desempl_barrio():
 def total_barrio():
     #box to select the page
     pag_lines = [0,1,2,3,4,5,6,7] 
-    n = st.selectbox("Selecciona la página para ver los datos de población total según el barrio:", pag_lines)
+    n = st.selectbox("Selecciona la pagina para ver los datos de población total según el barrio:", pag_lines)
 
     #getting the data
     df = pd.DataFrame.from_dict(get_neighborhoods(n))
@@ -180,3 +280,101 @@ def sub_pl():
     fig.update_yaxes(title_text="<b>Desempleo</b>", secondary_y=True)
 
     st.write(fig)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#--------------------------------------------GRÁFICOS POBLACIÓN-------------------------------
+#x barrio y number c edad
+#def edades_poblacion():
+
+#    distrito = st.selectbox("Selecdfjgnlkciondsfnma mdfbn mc distrito:", all_district())
+#    pag_lines = [0,1,2,3,4,5,6,7] 
+#    n = st.selectbox("Seleccionadsijklhjdscbhdnfjksak para ver los datos de la demanda de empleo en función del barrio:", pag_lines)
+
+#    a = get_population_neighborhood(distrito, n)
+#    df = pd.DataFrame(a)
+#    distritos = []
+#    barrio = []
+#    genero = []
+#    total = []
+#    edad = []
+#    for _, data in df.iterrows():
+#        for dicc in df["results"]:
+#            for c in dicc:
+#                if c == "District.Name":
+#                    distritos.append(dicc["District.Name"])
+#                elif "Number" == c:
+#                    total.append(dicc["Number"])
+#                elif "Neighborhood.Name" == c:
+#                    barrio.append(dicc["Neighborhood.Name"])
+#                elif "Gender" == c:
+#                    genero.append(dicc["Gender"])     
+#                elif "Age" == c:
+#                    edad.append(dicc["Age"])    
+
+#    fig = pe.bar(df, x=barrio, y=total, color=edad, labels={'x':'Barrio', 'y': 'Población total'})               
+#    st.write(fig) 
+
+
+
+"""def genero_poblacion():
+
+    distrito = st.selectbox(" mdfbn mdfgc distrito:", all_district())
+    pag_lines = [0,1,2,3,4,5,6,7] 
+    n = st.selectbox("Seleccionadsijklhjdscbhsak vnc ver los datos de la demanda de empleo en función del barrio:", pag_lines)
+
+    a = get_population_neighborhood(distrito, n)
+    df = pd.DataFrame(a)
+    distritos = []
+    barrio = []
+    genero = []
+    total = []
+    edad = []
+    for _, data in df.iterrows():
+        for dicc in df["results"]:
+            for c in dicc:
+                if c == "District.Name":
+                    distritos.append(dicc["District.Name"])
+                elif "Number" == c:
+                    total.append(dicc["Number"])
+                elif "Neighborhood.Name" == c:
+                    barrio.append(dicc["Neighborhood.Name"])
+                elif "Gender" == c:
+                    genero.append(dicc["Gender"])     
+                elif "Age" == c:
+                    edad.append(dicc["Age"])    
+
+    fig = pe.bar(df, x=barrio, y=total, color=genero, labels={'x':'Barrio', 'y': 'Población total'})               
+    st.write(fig)
+"""
--- streamlit/data/st_data.py
+++ streamlit/data/st_data.py
@@ -2,9 +2,34 @@ import requests
 
 STURL="http://localhost:8000"
 
-def get_immigrants_nationality(nationality):
-    return requests.get(STURL + f"/nationality/immigrants/{nationality}").json()
+#selectboxes
+def all_nationality():
+    return requests.get(STURL + "/nationality/all").json()
 
+def all_district():
+    return requests.get(STURL + "/district/all").json()
+
+def all_neigh():
+    return requests.get(STURL + "/neighborhood/all").json()
+
+
+#immigrants
+def get_immigrants_neighborhood(district, num_page):
+    return requests.get(STURL + f"/district/immigrants/{district}/{num_page}").json()
+
+def get_immigrants_nationality(nationality, num_page):
+    return requests.get(STURL + f"/nationality/immigrants/{nationality}/{num_page}").json()
+
+
+#unemployment
+def get_unemployed_neighborhood(district, num_page):
+    return requests.get(STURL + f"/district/unemployment/{district}/{num_page}").json()
+
+#population
+def get_population_neighborhood(district, num_page):
+    return requests.get(STURL + f"/district/population/{district}/{num_page}").json()
+
+#total
 def get_neighborhoods(num_page):
     return requests.get(STURL + f"/neighborhoods/{num_page}").json()
     
@@ -17,8 +42,6 @@ def get_total_unemployed(neighborhood):
 def get_total(neighborhood):
     return requests.get(STURL + f"/totalpopulation/{neighborhood}").json()
 
-def all_neigh():
-    return requests.get(STURL + "/neighborhood/all").json()
 
 def immigrants_nationality(nationality):
     return requests.get(STURL + f"/nationality/immigrants/{nationality}").json()
